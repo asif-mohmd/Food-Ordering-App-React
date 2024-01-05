8359f2e40eb41208db2b2011b93c26fc493b091d
asif-mohmd <asifasifpsps@gmail.com>
2024-01-05 17:33:33 +0530

    header created and basic styling completed

--- App.js
+++ App.js
@@ -2,14 +2,37 @@ import React from "react"
 import  ReactDOM  from "react-dom/client"
 
 
-const Heading = () =>(
-    <div id="container">
-         <h1>Hellow World</h1>
-    </div>
-      
-)
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logoContainer">
+                <img  className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMNdcsoiIdR5WbgEeSXjKPuEKJ3FHASgnWTw&usqp=CAU" alt="" />
+            </div>
 
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+
+        </div>
+
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<Heading/>)
+root.render(<AppLayout/>)
