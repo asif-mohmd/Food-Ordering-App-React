9be9f0875779e8d51c2161cc44258c7805f1781f
asif-mohmd <asifasifpsps@gmail.com>
2024-01-05 19:54:11 +0530

    created a basic card

--- App.js
+++ App.js
@@ -22,11 +22,33 @@ const Header = () => {
     )
 }
 
+const RestaurantCard = () =>{
+    return(
+        <div className="res-card" style={{backgroundColor:"rgb(249, 249, 248)"}}>
+          <img className="RestaurantCardImg" src="https://b.zmtcdn.com/data/pictures/chains/4/50574/3faca0f3f3635c659e15c0504873c503_o2_featured_v2.jpg" alt="" />
+            <h3 className="RestaurantCardName">Meghana Foods</h3>
+        </div>
+    )
+
+}
+
+const Body = () =>{
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header/>
+            <Body/>
 
         </div>
 
