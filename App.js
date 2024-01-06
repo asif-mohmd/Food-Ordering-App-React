60db8b10a4f5bb5a0c60da7d8258b853d21b93c3
asif-mohmd <asifasifpsps@gmail.com>
2024-01-06 14:09:08 +0530

    code optimized and formatted

--- App.js
+++ App.js
@@ -546,9 +546,8 @@ const Body = () => {
       <div className="res-container">
 
         {resList.map((restaurant) => (
-          <RestaurantCard resData={restaurant} />
+          <RestaurantCard key={restaurant.info.id}  resData={restaurant} />
         ))}
-
       </div>
     </div>
   );
