3f1e2bb952e9dfdbd999ccbd12d652f9c1eee97f
asif-mohmd <asifasifpsps@gmail.com>
2024-01-06 11:57:13 +0530

    fetched data like fetching from API and displayed

--- App.js
+++ App.js
@@ -22,28 +22,633 @@ const Header = () => {
     )
 }
 
-const RestaurantCard = () =>{
+const RestaurantCard = (props) =>{
+    const  {resData} = props
+    console.log(props)
     return(
         <div className="res-card" style={{backgroundColor:"rgb(249, 249, 248)"}}>
           <img className="RestaurantCardImg" src="https://b.zmtcdn.com/data/pictures/chains/4/50574/3faca0f3f3635c659e15c0504873c503_o2_featured_v2.jpg" alt="" />
             <h3 className="RestaurantCardName">Meghana Foods</h3>
-            <h4>Chicking, KFC</h4>
-            <h4>4.4 stars</h4>
+            <h4>{resData.info.name}</h4>
+            <h4>{resData.info.cuisines}</h4>
+            <h4>{resData.info.cuisines}</h4>
+            <h4>{resData.info.cuisines}</h4>
+            <h4>{resData.info.cuisines}</h4>
             <h5>20 minutes</h5>
         </div>
     )
 
 }
 
+const resList = [
+    {
+      "info": {
+        "id": "334867",
+        "name": "Chaayos Chai+Snacks=Relax",
+        "cloudinaryImageId": "cace805a6ba74137571d0f7ac92302b1",
+        "locality": "5th Sector",
+        "areaName": "HSR Layout",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+          "Bakery",
+          "Beverages",
+          "Chaat",
+          "Desserts",
+          "Home Food",
+          "Italian",
+          "Maharashtrian",
+          "Snacks",
+          "Street Food",
+          "Sweets"
+        ],
+        "avgRating": 4.5,
+        "parentId": "281782",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 27,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "27 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-01-07 00:00:00",
+          "opened": true
+        },
+        "badges": {
+          "textExtendedBadges": [
+            {
+              "iconId": "guiltfree/GF_Logo_android_3x",
+              "shortDescription": "options available",
+              "fontColor": "#7E808C"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "",
+                    "fontColor": "#7E808C",
+                    "iconId": "guiltfree/GF_Logo_android_3x",
+                    "shortDescription": "options available"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹120"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/chaayos-chai-snacks-relax-5th-sector-hsr-layout-bangalore-334867",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "304",
+        "name": "A2B - Adyar Ananda Bhavan",
+        "cloudinaryImageId": "fwkkx0iaiuyojevcxxnp",
+        "locality": "Dr Puneeth Rajkumar Rd",
+        "areaName": "HSR Layout",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "South Indian",
+          "North Indian",
+          "Sweets",
+          "Chinese"
+        ],
+        "avgRating": 4.4,
+        "veg": true,
+        "parentId": "22",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 26,
+          "lastMileTravel": 3.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "26 mins",
+          "lastMileTravelString": "3.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-01-06 22:30:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "v1695133679/badges/Pure_Veg111.png",
+              "description": "pureveg"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "pureveg",
+                    "imageId": "v1695133679/badges/Pure_Veg111.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹100"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/a2b-adyar-ananda-bhavan-dr-puneeth-rajkumar-rd-hsr-layout-bangalore-304",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "247180",
+        "name": "Chai Point",
+        "cloudinaryImageId": "r0dtqmokbkmwvhtt7s2j",
+        "locality": "3rd Sector",
+        "areaName": "HSR Layout",
+        "costForTwo": "₹150 for two",
+        "cuisines": [
+          "Bakery",
+          "Beverages",
+          "Maharashtrian",
+          "Snacks",
+          "Street Food",
+          "South Indian",
+          "Punjabi",
+          "Chaat",
+          "Indian",
+          "American",
+          "North Indian",
+          "Fast Food",
+          "Desserts",
+          "Cafe",
+          "Healthy Food",
+          "Home Food"
+        ],
+        "avgRating": 4.5,
+        "parentId": "1607",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 27,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "27 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-01-06 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          "textExtendedBadges": [
+            {
+              "iconId": "guiltfree/GF_Logo_android_3x",
+              "shortDescription": "options available",
+              "fontColor": "#7E808C"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "",
+                    "fontColor": "#7E808C",
+                    "iconId": "guiltfree/GF_Logo_android_3x",
+                    "shortDescription": "options available"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹120"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/chai-point-3rd-sector-hsr-layout-bangalore-247180",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "17036",
+        "name": "Leon's - Burgers & Wings (Leon Grill)",
+        "cloudinaryImageId": "b2edbc28b7b8219d6e0a9c049ce06658",
+        "locality": "HSR Layout",
+        "areaName": "HSR Layout",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "American",
+          "Snacks",
+          "Turkish",
+          "Portuguese",
+          "Continental"
+        ],
+        "avgRating": 4.5,
+        "parentId": "371281",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 27,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "27 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-01-07 04:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹125 OFF",
+          "subHeader": "ABOVE ₹199",
+          "discountTag": "FLAT DEAL"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/leons-burgers-and-wings-leon-grill-hsr-layout-bangalore-17036",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "23682",
+        "name": "McDonald's",
+        "cloudinaryImageId": "bb7ae131544c7d37e10fc5faf76f09d6",
+        "locality": "HSR Layout",
+        "areaName": "HSR Layout",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Burgers",
+          "Beverages",
+          "Cafe",
+          "Desserts"
+        ],
+        "avgRating": 4.3,
+        "parentId": "630",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 25,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-01-07 02:45:00",
+          "opened": true
+        },
+        "badges": {
+          "textExtendedBadges": [
+            {
+              "iconId": "guiltfree/GF_Logo_android_3x",
+              "shortDescription": "options available",
+              "fontColor": "#7E808C"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "",
+                    "fontColor": "#7E808C",
+                    "iconId": "guiltfree/GF_Logo_android_3x",
+                    "shortDescription": "options available"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "20% OFF",
+          "subHeader": "UPTO ₹50"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/mcdonalds-hsr-layout-bangalore-23682",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "57283",
+        "name": "Burger King",
+        "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+        "locality": "HSR Layout",
+        "areaName": "Hsr Layout",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "avgRating": 4.2,
+        "parentId": "166",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 29,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "29 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-01-07 03:50:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹120"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+          },
+          "freedelMessage": "FREE DELIVERY",
+          "badgeType": "BADGE_TYPE_ONE_LITE"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/burger-king-hsr-layout-bangalore-57283",
+        "type": "WEBLINK"
+      }
+    }
+]
+
 const Body = () =>{
     return (
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
+                <RestaurantCard resData={resList[1]}/>
+                
+             
             </div>
         </div>
     )
