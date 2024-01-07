2201bdabbc5035cc431999e8492a157ad450b61a
asif-mohmd <asifasifpsps@gmail.com>
2024-01-07 22:03:19 +0530

    Login to Logout - vise versa

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,7 @@ import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
-    const [login,setLogin] = useState("Login")
-
-
+  const [btnName, setbtnName] = useState("Login");
 
   return (
     <div className="header">
@@ -15,11 +13,20 @@ const Header = () => {
       <div className="nav-items">
         <ul>
           <li>Home</li>
-          <li>About Us</li> 
+          <li>About Us</li>
           <li>Contact us</li>
           <li>Cart</li>
-        
-          <button className="login" onClick={()=>{setLogin("Logout")}}>{login}</button>
+
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ?
+                setbtnName("Logout") :
+                setbtnName("Login")
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
