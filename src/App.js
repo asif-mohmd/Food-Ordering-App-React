e121af9791dcbf7401d672d44fc094ffbbf04bcd
asif-mohmd <asifasifpsps@gmail.com>
2024-01-08 19:21:15 +0530

    fetched data from API but not rendered

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Body from "./components/Body";
 const AppLayout = () => {
   return (
     <div className="app">
+    
       <Header />
       <Body />
     </div>
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurant] = useState(resList);
 
+
+useEffect(()=>{
+
+fetchData()
+},[])
+
+const fetchData = async () =>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89037501599536&lng=77.64229110894304&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+  
+    const json = await data.json()
+    console.log(json)
+
+}
+
+console.log("Body rendered")
   return (
     <div className="body">
       <div className="filter">
