025fffd3f91e15d71d9dfcda068915230b2ea067
asif-mohmd <asifasifpsps@gmail.com>
2024-01-23 21:19:52 +0530

    code formatted 3

--- src/components/Grocery.js
+++ src/components/Grocery.js
@@ -1,8 +1,5 @@
-const Grocery = () =>{
- 
-    return (
-        <h1>This is the Grocery Store</h1>
-    )
-}
+const Grocery = () => {
+  return <h1>This is the Grocery Store</h1>;
+};
 
-export default Grocery
+export default Grocery;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,17 @@
-import { useState , useContext} from "react";
+import { useState, useContext } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import UserContext from "../utils/UserContext";
 import { useSelector } from "react-redux";
 
-
 const Header = () => {
   const [btnName, setbtnName] = useState("Login");
 
-  const {loggedInUser} = useContext(UserContext)
+  const { loggedInUser } = useContext(UserContext);
 
   // Selector (Subscribing our store using Selector)
-  const cartItems = useSelector((store)=> store.cart.items)
-  
-    
+  const cartItems = useSelector((store) => store.cart.items);
+
   return (
     <div className="flex justify-between bg-slate-100  shadow-lg m-2">
       <div className="logoContainer m-5">
@@ -22,19 +20,27 @@ const Header = () => {
 
       <div className="flex items-center">
         <ul className="flex p-4 m-4">
-          <li className="px-2"><Link to={"/"}>Home</Link></li>
-          <li className="px-2"><Link to={"/about"}>About Us</Link></li>
-          <li className="px-2"><Link to={"/contact"}>Contact Us</Link></li>
-          <li className="px-2"><Link to={"/grocery"}>Grocery</Link></li>
-          <li className="px-2"><Link to={"/cart"}>Cart ({cartItems.length} items)</Link></li>
+          <li className="px-2">
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li className="px-2">
+            <Link to={"/about"}>About Us</Link>
+          </li>
+          <li className="px-2">
+            <Link to={"/contact"}>Contact Us</Link>
+          </li>
+          <li className="px-2">
+            <Link to={"/grocery"}>Grocery</Link>
+          </li>
+          <li className="px-2">
+            <Link to={"/cart"}>Cart ({cartItems.length} items)</Link>
+          </li>
 
           <button
             className="login pl-4"
             onClick={() => {
-              btnName === "Login" ?
-                setbtnName("Logout") :  
-                setbtnName("Login")
-            }} 
+              btnName === "Login" ? setbtnName("Logout") : setbtnName("Login");
+            }}
           >
             {btnName}
           </button>
